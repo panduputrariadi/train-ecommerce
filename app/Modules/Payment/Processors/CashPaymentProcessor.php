d2a222037ad79764ce5f994c18b4fc6557b8a96e
panduputragit <put.rariadi1144@gmail.com>
2025-10-24 09:06:30 +0800

    refactor: refactor function handle and split it to be the other function

--- app/Modules/Payment/Processors/CashPaymentProcessor.php
+++ app/Modules/Payment/Processors/CashPaymentProcessor.php
@@ -12,6 +12,7 @@ use Illuminate\Validation\ValidationException;
 
 class CashPaymentProcessor extends BasePaymentProcessor
 {
+
     /**
      * Handle cash payment
      *
@@ -22,13 +23,39 @@ class CashPaymentProcessor extends BasePaymentProcessor
      */
     public function handle(Order $order, CreatePaymentDto $dto): Payment
     {
+        $this->validatePayment($order, $dto);
+
+        $payment = $this->createPayment($order, $dto);
+
+        $this->updateOrderStatus($order);
+
+        return $payment;
+    }
+
+    /**
+     * Validate the payment information.
+     *
+     * @throws ValidationException
+     */
+    protected function validatePayment(Order $order, CreatePaymentDto $dto): void
+    {
         if ($dto->paidAmount < $order->grand_total) {
             throw ValidationException::withMessages([
                 'paid_amount' => 'Paid amount is less than grand total',
             ]);
         }
+    }
 
-        $payment = Payment::create([
+    /**
+     * Create a payment for the given order and payment information.
+     *
+     * @param  Order $order
+     * @param  CreatePaymentDto $dto
+     * @return Payment
+     */
+    protected function createPayment(Order $order, CreatePaymentDto $dto): Payment
+    {
+        return Payment::create([
             'order_id' => $order->id,
             'payment_method_id' => $dto->paymentMethodId,
             'amount' => $dto->paidAmount,
@@ -37,9 +64,15 @@ class CashPaymentProcessor extends BasePaymentProcessor
             'created_by' => Auth::id(),
             'paid_at' => now(),
         ]);
+    }
 
+    /**
+     * Update the status of the order to COMPLETED.
+     *
+     * @param Order $order
+     */
+    protected function updateOrderStatus(Order $order): void
+    {
         $order->update(['status' => OrderStatus::COMPLETED]);
-
-        return $payment;
     }
 }
--- app/Modules/Payment/Processors/TransferPaymentProcessor.php
+++ app/Modules/Payment/Processors/TransferPaymentProcessor.php
@@ -16,7 +16,6 @@ class TransferPaymentProcessor extends BasePaymentProcessor
 {
     use HandlePhotoUploadTrait;
 
-
     /**
      * Handle transfer payment
      *
@@ -26,39 +25,84 @@ class TransferPaymentProcessor extends BasePaymentProcessor
      */
     public function handle(Order $order, CreatePaymentDto $dto): Payment
     {
-        $bankAccount = BankAccount::findOrFail($dto->bankAccountId);
-
+        $bankAccount = $this->validateBankAccount($dto);
         $dto->paidAmount = $order->grand_total;
 
-        $payment = Payment::create([
+        $payment = $this->createPayment($order, $dto, $bankAccount);
+
+        if ($dto->evidenceFile) {
+            $this->attachPaymentReceipt($payment, $dto);
+        }
+
+        $this->updateOrderStatus($order);
+
+        return $payment;
+    }
+
+    /**
+     * Validate the bank account of the given payment information.
+     *
+     * @param CreatePaymentDto $dto
+     * @return BankAccount
+     * @throws \Illuminate\Database\Eloquent\ModelNotFoundException
+     */
+    protected function validateBankAccount(CreatePaymentDto $dto): BankAccount
+    {
+        return BankAccount::findOrFail($dto->bankAccountId);
+    }
+
+    /**
+     * Create a payment for the given order and payment information.
+     *
+     * @param Order $order
+     * @param CreatePaymentDto $dto
+     * @param BankAccount $bankAccount
+     * @return Payment
+     */
+    protected function createPayment(Order $order, CreatePaymentDto $dto, BankAccount $bankAccount): Payment
+    {
+        return Payment::create([
             'order_id' => $order->id,
             'payment_method_id' => $dto->paymentMethodId,
-            'bank_account_id' => $dto->bankAccountId,
+            'bank_account_id' => $bankAccount->id,
             'amount' => $dto->paidAmount,
             'status' => PaymentStatus::PENDING,
             'note' => $dto->notes,
             'created_by' => Auth::id(),
             'paid_at' => now(),
         ]);
+    }
 
-        if ($dto->evidenceFile) {
-            $photoPath = $this->uploadPhoto(
-                photo: $dto->evidenceFile,
-                directory: 'payments',
-                entityId: $payment->id,
-                nameForSlug: 'payment-evidence'
-            );
+    /**
+     * Attach payment receipt to the given payment.
+     *
+     * @param Payment $payment
+     * @param CreatePaymentDto $dto
+     */
+    protected function attachPaymentReceipt(Payment $payment, CreatePaymentDto $dto): void
+    {
+        $photoPath = $this->uploadPhoto(
+            photo: $dto->evidenceFile,
+            directory: 'payments',
+            entityId: $payment->id,
+            nameForSlug: 'payment-evidence'
+        );
 
-            PaymentReceipt::create([
-                'payment_id' => $payment->id,
-                'file_path' => $photoPath,
-                'mime_type' => $dto->evidenceFile->getMimeType(),
-                'uploaded_at' => now(),
-            ]);
-        }
+        PaymentReceipt::create([
+            'payment_id' => $payment->id,
+            'file_path' => $photoPath,
+            'mime_type' => $dto->evidenceFile->getMimeType(),
+            'uploaded_at' => now(),
+        ]);
+    }
 
+    /**
+     * Update the status of the order to PROCESSING.
+     *
+     * @param Order $order
+     */
+    protected function updateOrderStatus(Order $order): void
+    {
         $order->update(['status' => OrderStatus::PROCESSING]);
-
-        return $payment;
     }
 }
